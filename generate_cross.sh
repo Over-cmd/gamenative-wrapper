b9e62a301ff3e4f12ec43b0301a55a620e4442c8
Over-cmd <sergioleonpines@gmail.com>
2026-09-02 09:06:57 +0200

    Update path handling for Clang in generate_cross.sh

--- generate_cross.sh
+++ generate_cross.sh
@@ -46,7 +46,7 @@ sys_root = '/usr/local/lib/android/sdk/ndk/28.2.13676358/toolchains/llvm/prebuil
 libdir = '/usr/local/lib/android/sdk/ndk/28.2.13676358/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/26'
 pkg_config_path = shims_path + '/lib/pkgconfig'
 pkg_config_libdir = shims_path + '/lib/pkgconfig'
-# 🟢 CORRECCIÓN SUPREMA DE INCLUSIÓN DIRECTA: Apuntamos los flags directamente al prefijo de instalación real de shims_64 para triturar el error de xf86drm.h sin duplicar archivos
+# 🟢 CORRECTOR DE PRECISIÓN DE RUTA: Alimentamos a Clang directamente desde el prefijo real aislado de shims_64 para no romper dependencias relativas ni duplicar carpetas
 [built-in options]
 c_args = ['--sysroot=/usr/local/lib/android/sdk/ndk/28.2.13676358/toolchains/llvm/prebuilt/linux-x86_64/sysroot', '-D__TERMUX__', '-I' + shims_path + '/include', '-I' + shims_path + '/include/libdrm']
 cpp_args = ['--sysroot=/usr/local/lib/android/sdk/ndk/28.2.13676358/toolchains/llvm/prebuilt/linux-x86_64/sysroot', '-D__TERMUX__', '-I' + shims_path + '/include', '-I' + shims_path + '/include/libdrm']
