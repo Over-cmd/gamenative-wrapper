d4d5bcb2df1e696978b5e0d1e0fdec482621361c
Over-cmd <sergioleonpines@gmail.com>
2026-09-06 12:01:11 +0200

    Update Meson configuration version to v50

--- build_inside.sh
+++ build_inside.sh
@@ -3,7 +3,7 @@ set -e
 
 BUILD_DIR="${1:-${BUILD_DIR:-build}}"
 
-# 🟢 1. MOTOR DE CONFIGURACIÓN DE MESON COMPATIBLE DUAL (CORREGIDO REAL v48)
+# 🟢 1. MOTOR DE CONFIGURACIÓN DE MESON COMPATIBLE DUAL (CORREGIDO REAL v50)
 if [ ! -d "${BUILD_DIR}" ]; then
   meson setup "${BUILD_DIR}" --cross-file /root/build-config/cross_file.txt \
       -Dcpp_rtti=false \
@@ -12,6 +12,7 @@ if [ ! -d "${BUILD_DIR}" ]; then
       -Dllvm=disabled \
       -Dshared-llvm=disabled \
       -Dplatforms=x11,android \
+      -Degl-native-platform=android \
       -Dgallium-drivers=panfrost \
       -Ddraw-use-llvm=false \
       -Dxmlconfig=disabled \
