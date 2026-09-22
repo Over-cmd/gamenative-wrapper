36ffe92f0b2f7b52750659454bc3ddd2df383a2a
Over-cmd <sergioleonpines@gmail.com>
2026-09-22 18:18:03 +0200

    Add KHR_surface_capabilities2 extension support
    Added support for KHR_surface_capabilities2 extension to improve Vulkan instance capabilities.

--- src/vulkan/wrapper/wrapper_instance.c
+++ src/vulkan/wrapper/wrapper_instance.c
@@ -19,13 +19,14 @@ const struct vk_instance_extension_table wrapper_instance_extensions = {
    
    /* 🚨 TUS EXTENSIONES PREMIUM DE PC INJECTADAS DE FORMA NATIVA: 
       Las añadimos respetando la sintaxis estricta de la tabla de Mesa. Esto rompe 
-      el límite de las 16 instancias y le da luz verde a DXVK y OpenGL (Zink) 
+      el límite de las 17 instancias y le da luz verde a DXVK y OpenGL (Zink) 
       para comunicarse con el servidor de pantalla sin dar pantallas negras. */
    .KHR_get_physical_device_properties2 = true,
    .KHR_external_fence_capabilities = true,
    .KHR_external_memory_capabilities = true,
    .KHR_external_semaphore_capabilities = true,
    .KHR_device_group_creation = true,
+   .KHR_surface_capabilities2 = true,
    .EXT_debug_report = true,
    .EXT_debug_utils = true,
 
